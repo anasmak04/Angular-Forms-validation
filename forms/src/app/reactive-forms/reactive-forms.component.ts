f42f1e49c44cb757be7fbb8076cb9c987dbc8488
anasmak04 <anasdev8@gmail.com>
2022-10-16 01:45:39 +0100

    add reactive forms

--- forms/src/app/reactive-forms/reactive-forms.component.ts
+++ forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormGroup, FormBuilder  } from '@angular/forms';
+import { Form, FormGroup, FormBuilder, Validators, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -8,9 +8,30 @@ import { Form, FormGroup, FormBuilder  } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
     myForm !: FormGroup;
-  constructor() { }
+
+    FullName = new FormControl('',Validators.required);
+    Email = new FormControl('',Validators.required);
+    comment = new FormControl('',Validators.required);
+
+  constructor(fb : FormBuilder) { 
+      this.myForm = fb.group({
+        FullName : this.FullName,
+        Email : this.Email,
+        comment : this.comment,
+      })
+  }
 
   ngOnInit(): void {
+  }
+
+  onsubmit(){
+    console.table([this.myForm.value]);
+
+  }
+
+  submit(){
+    alert('look in the console to see the result binding')
+
   }
 
 }
--- forms/src/app/reactive-forms/reactive-model/reactive-model.module.ts
+++ forms/src/app/reactive-forms/reactive-model/reactive-model.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsComponent } from '../reactive-forms.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomMaterialUiModule } from 'src/app/custom-material-ui/custom-material-ui.module';
 @NgModule({
   declarations: [
     ReactiveFormsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CustomMaterialUiModule,
   ],
   exports : [
-    ReactiveFormsComponent
+    ReactiveFormsComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    CustomMaterialUiModule
   ]
 })
 export class ReactiveModelModule { }
